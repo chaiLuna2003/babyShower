9761c602670d8a040da796d1b86f3fe8b5904571
AngelLuna <231241045@alumnos.utn.edu.mx>
2025-07-06 16:46:40 -0600

    Insercion del footer

--- votacion.js
+++ votacion.js
@@ -1,6 +1,6 @@
 
   function votar(opcion) {
-    alert(`¡Gracias por votar por ${opcion}! 💕`);
+    alert(`¡Gracias por votar por ${opcion}! `);
     // Aquí podrías luego integrar Firebase u otra lógica
   }
 
