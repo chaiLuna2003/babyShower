156ea4dca3f17090433cdcfc7d6c5ced17797715
AngelLuna <231241045@alumnos.utn.edu.mx>
2025-07-10 18:04:00 -0600

    Correcion en css de asistencias

---
+++ script.js
@@ -0,0 +1,8 @@
+  const toggleBtn = document.querySelector(".menu-toggle");
+      const menu = document.querySelector(".menu");
+
+      toggleBtn.addEventListener("click", () => {
+        menu.classList.toggle("active");
+      });
+
+
